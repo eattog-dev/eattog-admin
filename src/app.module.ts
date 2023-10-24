30276541e77e45d2e1172e4b344fea8964c01978
Emanuel <emanuel.ibanhes.contato@gmail.com>
2023-10-24 00:03:02 -0400

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { RestauranteModule } from './restaurante/restaurante.module';
       host: '127.0.0.1',
       port: 3306,
       username: 'root',
-      password: '',
+      password: 'root',
       database: 'eattog',
       autoLoadEntities: true,
       synchronize: true,
