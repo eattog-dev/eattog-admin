d6e82647b147b59d3f18654161eaf18f76a78cdd
Emanuel <emanuel.ibanhes.contato@gmail.com>
2023-11-24 12:51:39 -0400

    fix pass

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,7 +34,7 @@ import { JwtModule, JwtService } from '@nestjs/jwt';
       host: '127.0.0.1',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: '',
       database: 'eattog',
       autoLoadEntities: true,
       synchronize: true,
