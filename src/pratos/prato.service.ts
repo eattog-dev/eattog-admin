88b20c2ab774e5fb184e6b3c0e05e6a2ff480b25
Emanuel <emanuel.ibanhes.contato@gmail.com>
2023-11-29 17:33:20 -0400

    att

--- src/pratos/prato.service.ts
+++ src/pratos/prato.service.ts
@@ -186,13 +186,26 @@ export class PratoService {
 
     //retorna todos pratos de todas categorias separadamente 
     async getPratosPorCategoria(): Promise<CategoriaPratoEntity[]> {
-        return this.categoriaPratoRepository.find();
+        const categoriasAtivas = await this.categoriaPratoRepository
+            .createQueryBuilder('categoria')
+            .innerJoin('categoria.pratos', 'prato', 'prato.isActive = :isActive', { isActive: true })
+            .getMany();
+
+        return categoriasAtivas;
     }
 
+
     //retorna todos os pratos de uma categoria especifica 
-    async getPratosUmaCategoria(id: number): Promise<CategoriaPratoEntity> {
-        return await this.categoriaPratoRepository.findOneBy({ id: id });
+    async getPratosUmaCategoria(id: number): Promise<CategoriaPratoEntity | undefined> {
+        const categoriaPrato = await this.categoriaPratoRepository
+            .createQueryBuilder('categoria')
+            .leftJoinAndSelect('categoria.pratos', 'prato', 'prato.isActive = :isActive', { isActive: true })
+            .where('categoria.id = :id', { id })
+            .getOne();
+
+        return categoriaPrato;
     }
+
 
     //retorna a quantidade de categorias
     async qtdCategorias(categoriaID: number): Promise<number> {
